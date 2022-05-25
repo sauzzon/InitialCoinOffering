0ef23f112273aea8418185f50343eff8ed57c1ad
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-05-25 09:47:27 +0545

    ico done

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x7ceab9bb035a1b97317eaf8ddc3c5fa255a13336";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
